4e3b87f02f507cd718622a2a2d65f3ed864ae145
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2024-10-26 13:01:23 -0400

    player.py file added, player class added and player object created in main loop

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-import pygame, constants
+import pygame, constants, player
 
 
 def main():
     print(constants.START_MSG)
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-dt = 0
-running = True
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    running = True
+    gamer = player.Player(x=constants.SCREEN_WIDTH /2, y=constants.SCREEN_WIDTH/2)
 
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill(constants.BG_COLOR)
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+        # RENDER YOUR GAME HERE
+        gamer.draw(screen=screen)
 
-    # RENDER YOUR GAME HERE
+        # flip() the display to put your work on screen
+        pygame.display.flip()
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    dt = clock.tick(60) // 1000  # limits FPS to 60
+        dt = clock.tick(60) // 1000  # limits FPS to 60
 
 pygame.quit()
 
